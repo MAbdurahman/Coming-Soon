e5012c9b6517f2a309831cf529286c7aa0e00341
MAbdurahman <mdbdrrhm@gmail.com>
2018-10-26 00:49:24 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+/**
+ * Coming-Soon Scriptscript
+ * @auhor:  MAbdurrahman
+ * @date:  25 september 2018
+ * @version:  1.0.0
+ */
+
+  $(function () {
+      var typed5 = new Typed('#typing-text', {
+
+          strings: ['Mahdi','Mahdi Abdurrahman',
+              'Mahdi Abdurrahman Portfolio'],
+          typeSpeed: 120,
+          backSpeed: 70,
+          backDelay: 1000,
+          // cursorChar: '_',
+          showCursor: false,
+          shuffle: false,
+          smartBackspace: true,
+          loop: false,
+
+      });
+
+      /*** Countdown Timer */
+      $("#days")
+          .countdown("2018/12/07", function (event) {
+              $(this).html(event.strftime('%D <em>days</em>'))
+          });
+
+      $("#hours")
+          .countdown("2018/12/07", function (event) {
+              $(this).html(event.strftime('%H <em>hrs</em>'))
+          });
+
+      $("#minutes")
+          .countdown("2018/12/07", function (event) {
+              $(this).html(event.strftime('%M <em>mins</em>'))
+          });
+
+      $("#seconds")
+          .countdown("2018/12/07", function (event) {
+              $(this).html(event.strftime('%S <em>secs</em>'))
+          });
+
+
+  });
+
